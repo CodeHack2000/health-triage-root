f9a30b3b4c95919883e4e021e92f3169b7fed08b
Vladyslav Safronov <vladyslavsafronov@gmail.com>
2024-12-22 00:27:39 +0000

    Commit of core files

---
+++ start-tor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p /var/lib/tor/instance1 /var/lib/tor/instance2 /var/lib/tor/instance3 /var/lib/tor/instance4 /var/lib/tor/instance5 /var/lib/tor/instance6 /var/lib/tor/instance7 /var/lib/tor/instance8 /var/lib/tor/instance9 /var/lib/tor/instance10 /var/lib/tor/instance11 /var/lib/tor/instance12 /var/lib/tor/instance13 /var/lib/tor/instance14 /var/lib/tor/instance15 /var/lib/tor/instance16 /var/lib/tor/instance17 /var/lib/tor/instance18 /var/lib/tor/instance19 /var/lib/tor/instance20 /var/lib/tor/instance21 /var/lib/tor/instance22 /var/lib/tor/instance23 /var/lib/tor/instance24
+
+tor -f /etc/tor/torrc1 &
+tor -f /etc/tor/torrc2 &
+tor -f /etc/tor/torrc3 &
+tor -f /etc/tor/torrc4 &
+tor -f /etc/tor/torrc5 &
+tor -f /etc/tor/torrc6 &
+tor -f /etc/tor/torrc7 &
+tor -f /etc/tor/torrc8 &
+tor -f /etc/tor/torrc9 &
+tor -f /etc/tor/torrc10 &
+tor -f /etc/tor/torrc11 &
+tor -f /etc/tor/torrc12 &
+tor -f /etc/tor/torrc13 &
+tor -f /etc/tor/torrc14 &
+tor -f /etc/tor/torrc15 &
+tor -f /etc/tor/torrc16 &
+tor -f /etc/tor/torrc17 &
+tor -f /etc/tor/torrc18 &
+tor -f /etc/tor/torrc19 &
+tor -f /etc/tor/torrc20 &
+tor -f /etc/tor/torrc21 &
+tor -f /etc/tor/torrc22 &
+tor -f /etc/tor/torrc23 &
+tor -f /etc/tor/torrc24
+
+wait -n
---
+++ torrc/readme.txt
@@ -0,0 +1,4 @@
+Para criar uma hash:
+$ tor --hash-password minha_senha_segura
+
+A hash usamos no ficheiro torrc e a pass original devemos guardar para usar no código
